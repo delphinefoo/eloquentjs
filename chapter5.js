ee8d01e709d495df599544b1f4a6cba030167cae
Delphine Foo-Matkin <delphinef@gmail.com>
2015-05-12 12:42:52 -0700

    added answer to 4th exercise in chapter 5

--- chapter5.js
+++ chapter5.js
@@ -80,3 +80,62 @@ for (item in (groupBy(ancestry, grouper))) {
   console.log(item + ': ' + Math.round(average(groupBy(ancestry, grouper)[item])*10)/10);
 };
 
+// → 16: 43.5
+//   17: 51.2
+//   18: 52.8
+//   19: 54.8
+//   20: 84.7
+//   21: 94
+
+/*EVERY AND THEN SOME
+
+Arrays also come with the standard methods every and some.
+Both take a predicate function that, when called with an array element as argument,
+returns true or false. Just like && returns a true value only when the expressions on
+both sides are true, every returns true only when the predicate returns true for all 
+elements of the array. Similarly, some returns true as soon as the predicate returns 
+true for any of the elements. They do not process more elements than necessary—for example, 
+if some finds that the predicate holds for the first element of the array, it will not look 
+at the values after that.
+
+Write two functions, every and some, that behave like these methods, except that they take
+the array as their first argument rather than being a method.*/
+
+function every(array, func) {
+  for (var i = 0; i < array.length; i++) {
+    if (func(array[i]) == true && i == array.length -1) {
+      return true;
+    }
+    else if (func(array[i]) == true) {
+      continue;
+    }
+    else {
+      return false;
+    }
+  }
+}
+
+function some(array, func) {
+  check = false;
+  for (var i = 0; i < array.length; i++) {
+    if (func(array[i])){
+      check = true;
+      break;
+    }
+    else {
+      continue;
+    }
+  }
+  return check;
+}
+
+/*TESTS
+console.log(every([NaN, NaN, NaN], isNaN));
+// → true
+console.log(every([NaN, NaN, 4], isNaN));
+// → false
+console.log(some([NaN, 3, 4], isNaN));
+// → true
+console.log(some([2, 3, 4], isNaN));
+// → false
+*/
