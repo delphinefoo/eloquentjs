d40f2ae0c8b5ef7d984b3c0c5b842720385f2667
Delphine Foo-Matkin <delphinef@gmail.com>
2015-06-21 17:43:13 -0700

    Third exercise chapter 9

--- chapter9.js
+++ chapter9.js
@@ -73,7 +73,29 @@ var text = "'I'm the cook,' he said, 'it's my job.'";
 console.log(text.replace(/^'|(\W)'|'(\W)/g, "$1\"$2"));
 // Should return → "I'm the cook," he said, "it's my job."
 
+//NUMBERS AGAIN
 
+//A series of digits can be matched by the simple regular expression /\d+/.
+
+//Write an expression that matches only JavaScript-style numbers. 
+//It must support an optional minus or plus sign in front of the number, the decimal dot, 
+//and exponent notation—5e-3 or 1E10— again with an optional sign in front of the exponent. 
+//Also note that it is not necessary for there to be digits in front of or after the dot, but 
+//the number cannot be a dot alone. That is, .5 and 5. are valid JavaScript numbers, but a lone dot isn’t.
+
+var number = /^[+-]?(\.\d+|\d+\.|\d+)\d*(e[-\+]?\d+)?$/i;
+
+// Tests:
+["1", "-1", "+15", "1.55", ".5", "5.", "1.3e2", "1E-4",
+ "1e+12"].forEach(function(s) {
+  if (!number.test(s))
+    console.log("Failed to match '" + s + "'");
+});
+["1a", "+-1", "1.2.3", "1+1", "1e4.5", ".5.", "1f5",
+ "."].forEach(function(s) {
+  if (number.test(s))
+    console.log("Incorrectly accepted '" + s + "'");
+});
 
 
 
